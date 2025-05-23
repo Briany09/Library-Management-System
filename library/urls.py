5a9f41ef970cf66c85063d9f48b10e1ab2342f2a
Briany09 <brianystephen07@gmail.com>
2025-05-23 09:38:35 +0300

    user registration and user login

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import login_view
+from .views import login_view, register_view
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
     # path('dashboard/', dashboard_view, name='dashboard'),
 ]
--- library/views.py
+++ library/views.py
@@ -26,13 +26,19 @@ def register_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
         email = request.POST['email']
+
+        # Check if passwords match
+        if password != password_confirmation:
+            messages.error(request, 'Passwords do not match.')
+            return render(request, 'register.html')
 
         # create new user
         user = User.objects.create_user(username=username, password=password, email=email)
         user.save()
         messages.success(request, 'Registration successiful! You can now log in.')
-        return redirect('login')
+        return redirect('login') #Redirect to login after registration
     
     return render(request, 'library/register.html')
     
